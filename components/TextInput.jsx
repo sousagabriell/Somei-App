07b155d2808b18825a03aeb03110711fab86c58d
Gabriel Sousa <gabrieldesousa05@gmail.com>
2023-07-24 14:39:09 -0300

    fix:login

---
+++ components/TextInput.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { TextInput as Input } from 'react-native-paper';
+
+
+export function TextInput({ errorText, ...props }) {
+  
+  return (
+    
+    <View style={styles.container}>
+      <Input
+        style={styles.input}
+        underlineColor="transparent"
+        mode="outlined"
+        {...props}
+      />
+      {errorText ? <Text style={styles.error}>{errorText}</Text> : null}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    marginVertical: 12,
+  },
+  error: {
+    fontSize: 14,
+    paddingHorizontal: 4,
+    paddingTop: 4,
+  },
+  label: {
+    color: '#fff',
+  },
+  input:{
+    borderRadius: 20,
+   
+  }
+});
